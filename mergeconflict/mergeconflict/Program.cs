0ded0f0b7051f47d4f44c94ff083375d92bbc780
Brian3010 <48544476+Brian3010@users.noreply.github.com>
2019-03-27 01:18:42 +1100

    Add files via upload

---
+++ mergeconflict/mergeconflict/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+namespace mergeconflict
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 5, num2 = 5, result;
+
+            result = num1 + num2;
+            Console.WriteLine("the addition of 5 + 5 is {0}", result);
+        }
+    }
+}
